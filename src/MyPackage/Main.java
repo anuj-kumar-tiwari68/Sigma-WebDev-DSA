53217df7334bfd172aa9090f31586464e5037267
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2026-08-12 00:57:50 +0530

    Add Java basics programs

---
+++ src/MyPackage/Main.java
@@ -0,0 +1,20 @@
+package MyPackage;
+import java.util.*;
+public class Main {
+    static void main() {
+//        System.out.println("Hello World");
+//        String name = "Cap";
+//        System.out.println(name);
+//        //variables
+//        int a = 10;
+//        int b = 20;
+//        int c= a+b;
+//        System.out.println(c);
+        Scanner sc = new Scanner(System.in);// Scanner class ki object banai
+        //String naam = sc.next();// wahan se input liya
+        String naam = sc.nextLine();
+        //nextInt()
+        //nextFloat()
+        System.out.println(naam);
+    }
+}
