a4e1795fbc22a56f1760642ce22d10e7e9c6085a
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2026-08-12 10:44:45 +0530

    Loops and questions based on them.

---
+++ src/MyPackage/forloop.java
@@ -0,0 +1,19 @@
+package MyPackage;
+
+public class forloop {
+    static void main(String[] args) {
+//        for(int ctr = 0;ctr <11;ctr++){
+//            System.out.println(ctr);
+//        }
+//        int i = 0;
+//        while (i<11){
+//            System.out.println(i);
+//            i++;
+//        }
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i<11);
+    }
+}
---
+++ src/MyPackage/sumusingfor.java
@@ -0,0 +1,14 @@
+package MyPackage;
+import java.util.*;
+public class sumusingfor {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0;i<=n;i++){
+
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
