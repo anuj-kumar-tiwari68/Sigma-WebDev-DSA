c5116d99c31147e58135f5b43cc8f0fe7f5956dc
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2026-08-12 23:55:14 +0530

    Medium level patterns.

---
+++ src/MyPackage/patterns4.java
@@ -0,0 +1,16 @@
+package MyPackage;
+
+public class patterns4 {
+    static void main(String[] args) {
+        int n = 5;
+        int number = 1;
+        for (int i = 0;i <=n; i++ ){
+            for (int j = 1; j<=i; j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+
+        }
+    }
+}
