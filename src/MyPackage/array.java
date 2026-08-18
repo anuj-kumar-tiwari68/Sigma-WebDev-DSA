eb986f205020bff807dfbd1309d521205c5a4a21
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2026-08-19 01:06:55 +0530

    Arrays

---
+++ src/MyPackage/array.java
@@ -0,0 +1,14 @@
+package MyPackage;
+
+public class array {
+    static void main(String[] args) {
+        int[] marks = new int[3];
+//        int markss[] = new int[5];
+        marks[0] = 33;
+        marks[1] = 44;
+        marks[2] = 55;
+        for (int i = 0; i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
